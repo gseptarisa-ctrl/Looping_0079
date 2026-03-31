1d717618890e83a21696535bb9082e25a6a83d96
gseptarisa-ctrl <gseptarisa@gmail.com>
2026-03-31 16:19:21 +0700

    Menambahkan input dan rerata

--- LogikaAnd.cpp
+++ LogikaAnd.cpp
@@ -1,3 +1,14 @@
 // Logika AND
 #include <iostream>
 using namespace std;
+
+int main(){
+    float nilB,nilM,rerata;
+    string status;
+
+    cout<<"Masukkan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin>>nilB;
+
+    rerata = (nilB+nilM)/2;
